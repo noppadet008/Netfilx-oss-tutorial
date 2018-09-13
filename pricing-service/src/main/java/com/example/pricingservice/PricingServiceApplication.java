69df18df8b2e7a859ff4512ebe6491d384c3af69
Noppadet Vivatchotikul <noppadet-ice@hotmail.com>
2018-09-13 17:57:21 +0700

    init repo

---
+++ pricing-service/src/main/java/com/example/pricingservice/PricingServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.pricingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PricingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricingServiceApplication.class, args);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Microservice tutorial 
+
+Thx to
+https://developers.ascendcorp.com/%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87-microservices-%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2-netflix-oss-%E0%B9%81%E0%B8%A5%E0%B8%B0-spring-cloud-2678667d9dbc
